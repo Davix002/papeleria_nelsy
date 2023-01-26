96cee2f9acab21811d9344f45d5b0682be223689
David Valencia <davidvalencia0526@gmail.com>
2023-01-25 21:22:46 -0500

    pruebas_dotenv

--- connection.php
+++ connection.php
@@ -1,18 +1,12 @@
 <?php
-
-    require_once __DIR__ . '/vendor/autoload.php';
+    require __DIR__ . '/vendor/autoload.php';
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 
     /*$host = "localhost";
     $usuario = "root";
     $password = "";
-    $dbnombre = "db_nelsy";
-
-    echo $_ENV['MYSQLHOST'];
-    echo $_ENV['MYSQLUSER'];
-    echo $_ENV['MYSQLPASSWORD'];
-    echo $_ENV['MYSQLDATABASE'];*/
+    $dbnombre = "db_nelsy";*/
 
     $host = $_ENV['MYSQLHOST'];
     $usuario = $_ENV['MYSQLUSER'];
