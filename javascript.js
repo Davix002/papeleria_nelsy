b09dab6fef9a2857ecc9cf19947e444309b238db
David Valencia <davidvalencia0526@gmail.com>
2023-01-26 08:10:23 -0500

    arreglo error calcular precio

--- javascript.js
+++ javascript.js
@@ -52,7 +52,7 @@
     }
 
     document.getElementById("costo").addEventListener("change", calcular_precio);
-    document.getElementById("p_uni").addEventListener("change", calcular_precio_mayor);
+    document.getElementById("p_uni").addEventListener("change", calcular_precio);
                 
     function roundToMultipleOf100(value) {
         return Math.round(value / 100) * 100;
